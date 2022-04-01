00a025c1a06b171a33c8ca4dbf99126005b0ad0f
Eric Taylor <eric.taylor1012@gmail.com>
2022-03-31 23:12:48 -0700

    final working code

--- script.js
+++ script.js
@@ -6,78 +6,57 @@
 // returns should be generated according to prompts
 
 //possible generated passwords
-var characters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s",'t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9','0','!',',','@','#','$','%','^','&','*','(',')','_','+','>','?','<',];
-var lcase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s",'t','u','v','w','x','y','z'];
-var ucase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var numbers = ['1','2','3','4','5','6','7','8','9','0'];
-var symbols = ['!',',','@','#','$','%','^','&','*','(',')','_','+','>','?','<',];
-var pLength = n;
-var passwordOutput = [];
+// var characters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s",'t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9','0','!',',','@','#','$','%','^','&','*','(',')','_','+','>','?','<',];
 
 
 
 
 
+function generatePassword(){
 
-//ask user for password criteria
-var passwordLength = prompt("Choose password length between 8 and 128", "10");
-var responseLower = prompt("Would you like to use lowercase letters for your password?", "y or n");
-if (responseLower === "y"){
-  passwordOutput.push(lcase)
-  console.log(passwordOutput)
-} else {
-} 
+     var lcase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s",'t','u','v','w','x','y','z'];
+     var ucase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+     var numbers = ['1','2','3','4','5','6','7','8','9','0'];
+     var symbols = ['!',',','@','#','$','%','^','&','*','(',')','_','+','>','?','<',];
+     var passwordOutput = [];
 
-var responseUpper = prompt("Would you like to use uppercase letters for your password?", "y or n");
-if (responseUpper === "y"){
-  passwordOutput.push(ucase)
-  console.log(passwordOutput)
-} else {
-} 
+     //ask user for password criteria
+     var passwordLength = prompt("Choose password length between 8 and 128");
+     
+     var responseLower = confirm("Would you like to use lowercase letters for your password?");
+     var responseUpper = confirm("Would you like to use uppercase letters for your password?");
+     var responseNumber = confirm("Would you like to use numbers for your password?");
+     var responseSymbol = confirm("Would you like to use special characters letters for your password?");
 
-var responseNumber = prompt("Would you like to use numbers for your password?", "y or n")
-if (responseNumber === "y"){
-  passwordOutput.push(number)
-  console.log(passwordOutput)
-} else {
-} 
+     if (responseLower === true){
+     passwordOutput.push(...lcase);
+     }
 
-var responseSymbol = prompt("Would you like to use special characters letters for your password?", "y or n");
-if (responseSymbol === "y"){
-  passwordOutput.push(symbols)
-  console.log(passwordOutput)
-} else {
-} 
+     if (responseUpper === true){
+     passwordOutput.push(...ucase);
+     }
 
-//var userRespone = [responseLower + responseUpper + responseNumber + responseSymbols],
+     if (responseNumber === true){
+     passwordOutput.push(...numbers);
+     } 
 
-//make font easier to compute
-responseLower = responseLower.toUpperCase();
-responseUpper = responseUpper.toUpperCase();
-responseNumber = responseNumber.toUpperCase();
-responseSymbol = responseSymbol.toUpperCase();
+     if (responseSymbol === true){
+     passwordOutput.push(...symbols);
+     } 
 
-// using userResponse to generate password criteria
-var indexCharacters = Math.floor(Math.random() * characters.length);
-var computerChoice = characters[indexCharacters];
-  
-for (let i = 0; i < n; i++)
-  console.log ("random")
+     var emptyString = [];
 
-//console.log("The computer chose " + computerChoice);
+     for (let i = 0; i < passwordLength; i++) {
+          emptyString.push(passwordOutput[(Math.floor(Math.random() * passwordOutput.length))]); 
+     }
 
-// Repeat the length of the password
+          console.log(emptyString)
+          return emptyString.join("");
 
+     // return emptyString.join("").toString();
 
-// Create filter for if then statement
-// If "lowercase" filter all lower case characters from index
-// If "uppercase" filter all upper case characters from index
-// If "symbols" filter all symbol characters from index
-// map items
-
-
-
-// if then statement for userResponse
+     
+};
 
 // Assignment Code, DO NOT EDIT ANTHING  BELOW THIS LINE
 var generateBtn = document.querySelector("#generate");
